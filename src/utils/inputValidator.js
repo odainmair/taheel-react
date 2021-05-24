87a39df2f3d2f672ca7c7e3a4cfa7ccd34c97090
Odai Nmair <odai@i.com>
2021-05-24 11:43:40 +0300

    fix issues

--- src/utils/inputValidator.js
+++ src/utils/inputValidator.js
@@ -9,3 +9,7 @@ export const checkEmailPattern = mail => {
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(mail);
 }
+export const checkMobilePattern = mobile => {
+    const regex = /^(5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$/;
+    return regex.test(mobile);
+}
