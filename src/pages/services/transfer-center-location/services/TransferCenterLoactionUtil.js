b369b46d99a45bfb09b06678b565a2e105af8e8d
noor <noor.khraisat@inspirejo.com>
2021-10-07 18:20:50 +0300

    --

--- src/pages/services/transfer-center-location/services/TransferCenterLoactionUtil.js
+++ src/pages/services/transfer-center-location/services/TransferCenterLoactionUtil.js
@@ -83,7 +83,7 @@ const NewAddressValidation = (values) => {
   if (!values.postalCode) {
     msg.postalCode = required;
   }else if(values.postalCode.length!=5){
-    msg.additionalNo = 'يجب ان يحتوي الرمز البريدي على خمس أرقام فقط'
+    msg.postalCode = 'يجب ان يحتوي الرمز البريدي على خمس أرقام فقط'
   }
 
   if (!values.additionalNo) {
